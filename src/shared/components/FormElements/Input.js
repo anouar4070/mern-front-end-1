84bcfab8770837f32304d00ef85a6092e2b7656e
anouar garbaa <g.anouar@yahoo.com>
2024-07-29 16:45:25 +0100

    add useCalback hook to NewPlace component

--- src/shared/components/FormElements/Input.js
+++ src/shared/components/FormElements/Input.js
@@ -1,4 +1,4 @@
-import React, { useReducer } from 'react';
+import React, { useReducer, useEffect } from 'react';
 
 import { validate } from '../../util/validators';
 import './Input.css';
@@ -10,6 +10,7 @@ const inputReducer = (state, action) => {
         ...state,
         value: action.val,
         isValid: validate(action.val, action.validators)
+        // the user input: action.val
       };
     case 'TOUCH': {
       return {
@@ -28,6 +29,16 @@ const Input = props => {
     isTouched: false,
     isValid: false
   });
+
+const {id, onInput} = props;
+const {value, isValid} = inputState;
+
+
+useEffect(() => {
+  onInput(id, value, isValid) 
+}, [id, value, isValid, onInput]);
+
+
 
   const changeHandler = event => {
     dispatch({
